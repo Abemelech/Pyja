aad291cf1ce24e1396ee04ead9071e13ff5718d9
Abemelech <abemelech.mesfin@gmail.com>
2022-03-10 17:13:18 -0600

    implemented a simple input function

--- main.py
+++ main.py
@@ -1,1 +1,22 @@
-print("Hello")
+
+def userInput():
+    ''' A function to get user input from user 
+    and return their input as a string''' 
+
+    # Takes in the user search input
+    userIn = input("Search: ")
+
+    # returns the input
+    return userIn   
+
+
+def main():
+    ''' The main function that will get executed'''
+
+    # Users search
+    userSearch = userInput()
+
+
+
+
+main()
